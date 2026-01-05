0b6a5b084f8acd610e288abd30bf2b4a962bc86e
shokhrukh7oo7 <shokh7oo70809@gmail.com>
2026-01-05 18:55:08 +0500

    typical it solution page moved

---
+++ wp-content/themes/connector/template-parts/solving-it-problems.php
@@ -0,0 +1,334 @@
+<?php
+/*
+    Template name: Решение IT задач в каждой отрасли
+*/
+get_header();
+?>
+
+<main>
+    <section class="solving-wrapper">
+        <div class="container">
+            <div class="it-service-wrapper">
+                <div class="it-service-header-wrapper">
+                    <h6>Решения</h6>
+                    <h1>Решение IT задач в каждой отрасли</h1>
+                    <p>
+                        Explore our IT services: innovative solutions, cutting-edge
+                        technology, and expert support to transform your business
+                        digitally.
+                    </p>
+                    <p class="breadcrumbs">
+                        Главная / Услуги /
+                        <span>Решение IT задач в каждой отрасли</span>
+                    </p>
+                </div>
+
+                <div class="banner-content-wrapper">
+                    <div class="banner-wrapper">
+                        <img src="<?= get_template_directory_uri() ?>/assets/images/solving-it-problems/banner.png" alt="image" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="inst-direction-container solving-direction-wrapper">
+        <div class="direction-container">
+            <div class="container">
+                <div class="directions-wrapper">
+                    <div class="directions-header-wrapper">
+                        <a href="#">Отрасли, которым мы помогаем</a>
+                        <h3>Решение ИТ-проблем в каждой отрасли</h3>
+                        <p>
+                            От стратегий роста для стартапов до помощи существующим
+                            бизнесам — мы сделали это все!
+                        </p>
+                    </div>
+
+                    <div class="directions-content-wrapper">
+                        <div class="item">
+                            <div class="item-content">
+                                <div class="icon-wrapper">
+                                    <img src="<?= get_template_directory_uri() ?>/assets/images/solving-it-problems/1.svg" alt="image" />
+                                </div>
+                                <h6>Агробизнес и сельское хозяйство</h6>
+                                <p>
+                                    Sustainable farming practices are enhancing productivity
+                                    while reducing environmental impact. Innovations in…
+                                </p>
+                            </div>
+                            <div class="btn-wrapper">
+                                <a href="#">Узнать больше</a>
+                                <img src="<?= get_template_directory_uri() ?>/assets/images/home/arrow-up.svg" alt="image" />
+                            </div>
+                        </div>
+
+                        <div class="item">
+                            <div class="item-content">
+                                <div class="icon-wrapper">
+                                    <img src="<?= get_template_directory_uri() ?>/assets/images/solving-it-problems/2.svg" alt="image" />
+                                </div>
+                                <h6>Производство и промышленность</h6>
+                                <p>
+                                    Industry 4.0 is revolutionizing manufacturing with smart
+                                    automation, IoT, and AI-driven quality…
+                                </p>
+                            </div>
+                            <div class="btn-wrapper">
+                                <a href="#">Узнать больше</a>
+                                <img src="<?= get_template_directory_uri() ?>/assets/images/home/arrow-up.svg" alt="image" />
+                            </div>
+                        </div>
+
+                        <div class="item">
+                            <div class="item-content">
+                                <div class="icon-wrapper">
+                                    <img src="<?= get_template_directory_uri() ?>/assets/images/solving-it-problems/3.svg" alt="image" />
+                                </div>
+                                <h6>Автомобиле-строение</h6>
+                                <p>
+                                    The automotive industry is shifting toward electrification
+                                    and autonomous mobility. EVs, AI-powered…
+                                </p>
+                            </div>
+                            <div class="btn-wrapper">
+                                <a href="#">Узнать больше</a>
+                                <img src="<?= get_template_directory_uri() ?>/assets/images/home/arrow-up.svg" alt="image" />
+                            </div>
+                        </div>
+
+                        <div class="item">
+                            <div class="item-content">
+                                <div class="icon-wrapper">
+                                    <img src="<?= get_template_directory_uri() ?>/assets/images/solving-it-problems/4.svg" alt="image" />
+                                </div>
+                                <h6>Банковский и финансовый сектор</h6>
+                                <p>
+                                    Fintech innovations, AI-driven risk assessment, and
+                                    blockchain technology are reshaping financial services.…
+                                </p>
+                            </div>
+                            <div class="btn-wrapper">
+                                <a href="#">Узнать больше</a>
+                                <img src="<?= get_template_directory_uri() ?>/assets/images/home/arrow-up.svg" alt="image" />
+                            </div>
+                        </div>
+                        <!--  -->
+                        <div class="item">
+                            <div class="item-content">
+                                <div class="icon-wrapper">
+                                    <img src="<?= get_template_directory_uri() ?>/assets/images/solving-it-problems/5.svg" alt="image" />
+                                </div>
+                                <h6>Здравоохранение и фармацевтика</h6>
+                                <p>
+                                    AI-powered diagnostics, personalized medicine, are
+                                    revolutionizing healthcare. Telemedicine and wearable
+                                    health tech…
+                                </p>
+                            </div>
+                            <div class="btn-wrapper">
+                                <a href="#">Узнать больше</a>
+                                <img src="<?= get_template_directory_uri() ?>/assets/images/home/arrow-up.svg" alt="image" />
+                            </div>
+                        </div>
+
+                        <div class="item">
+                            <div class="item-content">
+                                <div class="icon-wrapper">
+                                    <img src="<?= get_template_directory_uri() ?>/assets/images/solving-it-problems/6.svg" alt="image" />
+                                </div>
+                                <h6>Энергетика и добыча ископаемых</h6>
+                                <p>
+                                    The transition to renewable energy is accelerating with
+                                    advancements in solar, wind,
+                                </p>
+                            </div>
+                            <div class="btn-wrapper">
+                                <a href="#">Узнать больше</a>
+                                <img src="<?= get_template_directory_uri() ?>/assets/images/home/arrow-up.svg" alt="image" />
+                            </div>
+                        </div>
+
+                        <div class="item">
+                            <div class="item-content">
+                                <div class="icon-wrapper">
+                                    <img src="<?= get_template_directory_uri() ?>/assets/images/solving-it-problems/7.svg" alt="image" />
+                                </div>
+                                <h6>Медиа и шоубизнес</h6>
+                                <p>
+                                    Streaming platforms, AI-generated content, and immersive
+                                    technologies like AR/VR are reshaping entertainment.…
+                                </p>
+                            </div>
+                            <div class="btn-wrapper">
+                                <a href="#">Узнать больше</a>
+                                <img src="<?= get_template_directory_uri() ?>/assets/images/home/arrow-up.svg" alt="image" />
+                            </div>
+                        </div>
+
+                        <div class="item">
+                            <div class="item-content">
+                                <div class="icon-wrapper">
+                                    <img src="<?= get_template_directory_uri() ?>/assets/images/solving-it-problems/8.svg" alt="image" />
+                                </div>
+                                <h6>Торговля и ритейл</h6>
+                                <p>
+                                    Cloud computing, AI, and cybersecurity advancements are
+                                    driving digital transformation. Businesses are…
+                                </p>
+                            </div>
+                            <div class="btn-wrapper">
+                                <a href="#">Узнать больше</a>
+                                <img src="<?= get_template_directory_uri() ?>/assets/images/home/arrow-up.svg" alt="image" />
+                            </div>
+                        </div>
+                        <!--  -->
+                        <div class="item">
+                            <div class="item-content">
+                                <div class="icon-wrapper">
+                                    <img src="<?= get_template_directory_uri() ?>/assets/images/solving-it-problems/9.svg" alt="image" />
+                                </div>
+                                <h6>Телекоммуникации</h6>
+                                <p>
+                                    5G and fiber-optic networks are revolutionizing
+                                    connectivity, enabling faster data transmission and…
+                                </p>
+                            </div>
+                            <div class="btn-wrapper">
+                                <a href="#">Узнать больше</a>
+                                <img src="<?= get_template_directory_uri() ?>/assets/images/home/arrow-up.svg" alt="image" />
+                            </div>
+                        </div>
+
+                        <div class="item">
+                            <div class="item-content">
+                                <div class="icon-wrapper">
+                                    <img src="<?= get_template_directory_uri() ?>/assets/images/solving-it-problems/10.svg" alt="image" />
+                                </div>
+                                <h6>Образование и социальный сектор</h6>
+                                <p>
+                                    Digital government services, AI-driven policy analysis,
+                                    and blockchain transparency are enhancing public…
+                                </p>
+                            </div>
+                            <div class="btn-wrapper">
+                                <a href="#">Узнать больше</a>
+                                <img src="<?= get_template_directory_uri() ?>/assets/images/home/arrow-up.svg" alt="image" />
+                            </div>
+                        </div>
+
+                        <div class="item">
+                            <div class="item-content">
+                                <div class="icon-wrapper">
+                                    <img src="<?= get_template_directory_uri() ?>/assets/images/solving-it-problems/11.svg" alt="image" />
+                                </div>
+                                <h6>Склады, транспорт и логистика</h6>
+                                <p>
+                                    E-commerce, AI-driven marketing, and sustainable packaging
+                                    are reshaping the CPG sector. Personalization,…
+                                </p>
+                            </div>
+                            <div class="btn-wrapper">
+                                <a href="#">Узнать больше</a>
+                                <img src="<?= get_template_directory_uri() ?>/assets/images/home/arrow-up.svg" alt="image" />
+                            </div>
+                        </div>
+
+                        <div class="item">
+                            <div class="item-content">
+                                <div class="icon-wrapper">
+                                    <img src="<?= get_template_directory_uri() ?>/assets/images/solving-it-problems/12.svg" alt="image" />
+                                </div>
+                                <h6>Спорт</h6>
+                                <p>
+                                    Sustainable forestry practices, bio-based materials, and
+                                    AI-driven supply chain management are transforming…
+                                </p>
+                            </div>
+                            <div class="btn-wrapper">
+                                <a href="#">Узнать больше</a>
+                                <img src="<?= get_template_directory_uri() ?>/assets/images/home/arrow-up.svg" alt="image" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section>
+        <div class="container">
+            <div class="map-container-wrapper">
+                <img class="map-banner" src="<?= get_template_directory_uri() ?>/assets/images/solving-it-problems/map.png" alt="image" />
+
+                <div class="map-content-wrapper">
+                    <div class="left-wrapper">
+                        <h1>Где бы вы ни были, мы можем помочь</h1>
+                        <p>
+                            Наша задача - помочь вашему бизнесу работать быстрее и
+                            прибыльнее, взяв на себя все рутинные IT-задачи.
+                        </p>
+                    </div>
+                    <div class="right-wrapper">
+                        <div class="right-contact-wrapper">
+                            <h3>Бесплатная консультация</h3>
+                            <p>Позвоните или напишите в Телеграм</p>
+                            <div class="btn-wrapper">
+                                <a href="#">
+                                    Позвонить
+                                    <img src="<?= get_template_directory_uri() ?>/assets/images/solving-it-problems/phone.svg" alt="image" />
+                                </a>
+                                <a href="#">
+                                    Написать в Телеграм
+                                    <img src="<?= get_template_directory_uri() ?>/assets/images/solving-it-problems/telegram-logo.svg" alt="image" />
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section>
+        <div class="container">
+            <div class="solving-partner">
+                <div class="partner-swiper-wrapper">
+                    <!-- Slider main container -->
+                    <div class="swiper-container-solving-partner">
+                        <!-- Additional required wrapper -->
+                        <div class="swiper-wrapper">
+                            <div class="swiper-slide">
+                                <img src="<?= get_template_directory_uri() ?>/assets/images/solving-it-problems/p-1.png" alt="image" />
+                            </div>
+                            <div class="swiper-slide">
+                                <img src="<?= get_template_directory_uri() ?>/assets/images/solving-it-problems/p-2.png" alt="image" />
+                            </div>
+                            <div class="swiper-slide">
+                                <img src="<?= get_template_directory_uri() ?>/assets/images/solving-it-problems/p-3.png" alt="image" />
+                            </div>
+                            <div class="swiper-slide">
+                                <img src="<?= get_template_directory_uri() ?>/assets/images/solving-it-problems/p-4.png" alt="image" />
+                            </div>
+                            <div class="swiper-slide">
+                                <img src="<?= get_template_directory_uri() ?>/assets/images/solving-it-problems/p-5.png" alt="image" />
+                            </div>
+                            <div class="swiper-slide">
+                                <img src="<?= get_template_directory_uri() ?>/assets/images/solving-it-problems/p-6.png" alt="image" />
+                            </div>
+                        </div>
+                    </div>
+                    <div class="swiper-nav">
+                        <!-- If we need navigation buttons -->
+                        <div class="swiper-button-prev"></div>
+                        <div class="swiper-button-next"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</main>
+
+<?php
+get_footer();
+?>
---
+++ wp-content/themes/connector/template-parts/typical-it-solution.php
@@ -0,0 +1,158 @@
+<?php
+/*
+    Template name: Типовые IT решения
+*/
+get_header();
+?>
+
+<main>
+    <section class="solving-wrapper">
+        <div class="container">
+            <div class="it-service-wrapper">
+                <div class="it-service-header-wrapper">
+                    <h6>Решения</h6>
+                    <h1>Типовые IT решения</h1>
+                    <p>
+                        Explore our IT services: innovative solutions, cutting-edge
+                        technology, and expert support to transform your business
+                        digitally.
+                    </p>
+                    <p class="breadcrumbs">
+                        Главная / Услуги /
+                        <span>Типовые IT решения</span>
+                    </p>
+                </div>
+
+                <div class="banner-content-wrapper">
+                    <div class="banner-wrapper">
+                        <img src="<?= get_template_directory_uri() ?> /assets/images/typical-it-solution/banner.png" alt="image" />
+                    </div>
+
+                    <div class="banner-description-wrapper">
+                        <p>
+                            Click edit button to change this text. Lorem ipsum dolor sit
+                            amet, consectetur adipiscing elit. Ut elit tellus, luctus nec
+                            ullamcorper mattis, pulvinar dapibus leo. <br />
+                            <br />
+                            Click edit button to change this text. Lorem ipsum dolor sit
+                            amet, consectetur adipiscing elit. Ut elit tellus, luctus nec
+                            ullamcorper mattis, pulvinar dapibus leo.Click edit button to
+                            change this text. Lorem ipsum dolor sit amet, consectetur
+                            adipiscing elit. Ut elit tellus, luctus nec ullamcorper
+                            mattis, pulvinar dapibus leo.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="typical-card-container">
+        <div class="container">
+            <div class="card-section-wrapper">
+                <div class="row m-0">
+                    <div class="col-12 col-lg-6 p-2">
+                        <div class="card-item">
+                            <div class="card-image">
+                                <img src="<?= get_template_directory_uri() ?>/assets/images/home/card-1.png" alt="image" />
+                            </div>
+                            <div class="card-content">
+                                <div class="card-top-wrapper">
+                                    <h5>Как организовать работу с 1C на предприятии</h5>
+                                    <p>
+                                        Cloud computing revolutionizes IT by offering scalable
+                                        resources over the internet. Businesses can reduce
+                                        costs, enhance flexibility, and improve collaboration.
+                                        Popular services include AWS, Google…
+                                    </p>
+                                </div>
+                                <div class="card-bottom-wrapper">
+                                    <a href="#">
+                                        Узнать больше
+                                        <img src="<?= get_template_directory_uri() ?>/assets/images/home/arrow-up.svg" alt="image" />
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 col-lg-6 p-2">
+                        <div class="card-item">
+                            <div class="card-image">
+                                <img src="<?= get_template_directory_uri() ?>/assets/images/home/card-2.png" alt="image" />
+                            </div>
+                            <div class="card-content">
+                                <div class="card-top-wrapper">
+                                    <h5>Регулярное бэĸапирование данных и процессов</h5>
+                                    <p>
+                                        Cloud computing revolutionizes IT by offering scalable
+                                        resources over the internet. Businesses can reduce
+                                        costs, enhance flexibility, and improve collaboration.
+                                        Popular services include AWS, Google…
+                                    </p>
+                                </div>
+                                <div class="card-bottom-wrapper">
+                                    <a href="#">
+                                        Узнать больше
+                                        <img src="<?= get_template_directory_uri() ?>/assets/images/home/arrow-up.svg" alt="image" />
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 col-lg-6 p-2">
+                        <div class="card-item">
+                            <div class="card-image">
+                                <img src="<?= get_template_directory_uri() ?>/assets/images/home/card-3.png" alt="image" />
+                            </div>
+                            <div class="card-content">
+                                <div class="card-top-wrapper">
+                                    <h5>Связь филиалов предприятия через VPN</h5>
+                                    <p>
+                                        Cloud computing revolutionizes IT by offering scalable
+                                        resources over the internet. Businesses can reduce
+                                        costs, enhance flexibility, and improve collaboration.
+                                        Popular services include AWS, Google…
+                                    </p>
+                                </div>
+                                <div class="card-bottom-wrapper">
+                                    <a href="#">
+                                        Узнать больше
+                                        <img src="<?= get_template_directory_uri() ?>/assets/images/home/arrow-up.svg" alt="image" />
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 col-lg-6 p-2">
+                        <div class="card-item">
+                            <div class="card-image">
+                                <img src="<?= get_template_directory_uri() ?>/assets/images/home/card-4.png" alt="image" />
+                            </div>
+                            <div class="card-content">
+                                <div class="card-top-wrapper">
+                                    <h5>Мониторинг IT инфраструĸтуры 24/7</h5>
+                                    <p>
+                                        Cloud computing revolutionizes IT by offering scalable
+                                        resources over the internet. Businesses can reduce
+                                        costs, enhance flexibility, and improve collaboration.
+                                        Popular services include AWS, Google…
+                                    </p>
+                                </div>
+                                <div class="card-bottom-wrapper">
+                                    <a href="#">
+                                        Узнать больше
+                                        <img src="<?= get_template_directory_uri() ?>/assets/images/home/arrow-up.svg" alt="image" />
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</main>
+
+<?php
+get_footer();
+?>
