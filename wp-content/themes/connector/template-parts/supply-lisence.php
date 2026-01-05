1c5234684e5d4df9a83386832978345bd35960ed
shokhrukh7oo7 <shokh7oo70809@gmail.com>
2026-01-05 18:42:07 +0500

    supply lisence page moved

---
+++ wp-content/themes/connector/template-parts/supply-lisence.php
@@ -0,0 +1,209 @@
+<?php
+/*
+    Template name: Поставка лицензионного ПО
+*/
+get_header();
+?>
+
+<main>
+    <section>
+        <div class="container">
+            <div class="it-service-wrapper">
+                <div class="it-service-header-wrapper">
+                    <h6>Услуги</h6>
+                    <h1>Поставка лицензионного ПО</h1>
+                    <p>
+                        Команда IT специалистов #connector с 2009 года занимается
+                        техническим обслуживанием организаций в Ташкенте, Узбекистане
+                    </p>
+                    <p class="breadcrumbs">
+                        Главная / Услуги / <span>Поставка лицензионного ПО</span>
+                    </p>
+                </div>
+
+                <div class="banner-content-wrapper">
+                    <div class="banner-wrapper">
+                        <img src="<?= get_template_directory_uri() ?>/assets/images/supply-lisence/banner.png" alt="image" />
+                    </div>
+
+                    <div class="banner-description-wrapper">
+                        <p>
+                            Команда IT специалистов #connector предлагает не только
+                            услуги, но и поставку лицензионного программного обеспечения
+                            для организаций
+                            <br />
+                            <br />
+                            Для этого мы создали специальный сайт www.zetrix.uz на котором
+                            предлагаем широкий ассортимент современной техники и
+                            лицензионного программного обеспечения от ведущих
+                            производителей.
+                            <br />
+                            <br />
+                            ООО «ZETRIX» является торговой фирмой входящей в состав группы
+                            компаний #connector.
+                            <br />
+                            <br />
+                            Мы предлагаем широкий ассортимент лицензионного программного
+                            обеспечения от ведущих мировых вендоров (производителей),
+                            обеспечивая каждому клиенту профессиональный подход,
+                            индивидуальный подбор и полный цикл технического сопровождения
+                            на всех этапах сотрудничества.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="supply-lisence-wrpper">
+        <div class="additional-section-wrapper">
+            <div class="container">
+                <div class="additional-section-content">
+                    <h2>
+                        Широкий ассортимент лицензионного программного обеспечения
+                    </h2>
+                    <div class="additional-top-wrapper">
+                        <div class="additional-top-left-wrapper">
+                            <ul>
+                                <li>
+                                    <img src="<?= get_template_directory_uri() ?>/assets/images/home/check-circle.svg" alt="image" />
+                                    Системное ПО для серверов
+                                </li>
+
+                                <li>
+                                    <img src="<?= get_template_directory_uri() ?>/assets/images/home/check-circle.svg" alt="image" />
+                                    Лицензии для сетевого оборудования
+                                </li>
+
+                                <li>
+                                    <img src="<?= get_template_directory_uri() ?>/assets/images/home/check-circle.svg" alt="image" />
+                                    ПО для рабочих станций (компьютеры, ноутбуки)
+                                </li>
+
+                                <li>
+                                    <img src="<?= get_template_directory_uri() ?>/assets/images/home/check-circle.svg" alt="image" />
+                                    Антивирусное ПО
+                                </li>
+
+                                <li>
+                                    <img src="<?= get_template_directory_uri() ?>/assets/images/home/check-circle.svg" alt="image" />
+                                    ПО для IT безопасности
+                                </li>
+
+                                <li>
+                                    <img src="<?= get_template_directory_uri() ?>/assets/images/home/check-circle.svg" alt="image" />
+                                    Графическое ПО
+                                </li>
+
+                                <li>
+                                    <img src="<?= get_template_directory_uri() ?>/assets/images/home/check-circle.svg" alt="image" />
+                                    Специализированное ПО
+                                </li>
+
+                                <li>
+                                    <img src="<?= get_template_directory_uri() ?>/assets/images/home/check-circle.svg" alt="image" />
+                                    Программное обеспечение на заказ
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div class="additional-bottom-wrapper">
+                        <p>
+                            Мы реализуем исключительно лицензионное программное
+                            обеспечение, произведённое признанными мировыми лидерами в
+                            области информационных технологий. Являясь официальными
+                            партнёрами ведущих вендоров, мы осуществляем поставку
+                            лицензионного программного обеспечения по официальным
+                            легальным каналам дистрибуции. Такой подход гарантирует
+                            клиентам не только соответствие программных продуктов
+                            международным стандартам качества и безопасности, но и
+                            возможность получения полноценной технической поддержки как со
+                            стороны наших специалистов, так и непосредственно от
+                            производителей программного обеспечения
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="inst-direction-container it-system-container">
+        <div class="direction-container">
+            <div class="container">
+                <div class="directions-wrapper">
+                    <div class="directions-header-wrapper">
+                        <a href="#">Преимущества </a>
+                        <h3>Преимущества заказа проектной документации у нас</h3>
+                        <p>
+                            Наша комплексная IT-поддержка для вашей сложной сетевой среды
+                            включает серверы, рабочие станции, смартфоны, копиры, сканеры
+                            и принтеры.
+                        </p>
+                    </div>
+
+                    <div class="directions-content-wrapper">
+                        <div class="item">
+                            <div class="item-content">
+                                <div class="icon-wrapper">
+                                    <img src="<?= get_template_directory_uri() ?>/assets/images/supply-lisence/1.svg" alt="image" />
+                                </div>
+                                <h6>Свежий и чистый дизайн</h6>
+                                <p>
+                                    Тема оптимизирована для обеспечения вашим посетителям
+                                    ясного и великолепного опыта в плане UX/UI.
+                                </p>
+                            </div>
+                        </div>
+
+                        <div class="item">
+                            <div class="item-content">
+                                <div class="icon-wrapper">
+                                    <img src="<?= get_template_directory_uri() ?>/assets/images/supply-lisence/2.svg" alt="image" />
+                                </div>
+                                <h6>
+                                    Бесплатные обновления <br />
+                                    на всю жизнь
+                                </h6>
+                                <p>
+                                    При покупке темы вы получите бесплатные обновления на всю
+                                    жизнь навсегда.
+                                </p>
+                            </div>
+                        </div>
+
+                        <div class="item">
+                            <div class="item-content">
+                                <div class="icon-wrapper">
+                                    <img src="<?= get_template_directory_uri() ?>/assets/images/supply-lisence/3.svg" alt="image" />
+                                </div>
+                                <h6>Бесплатный плагин</h6>
+                                <p>
+                                    Все плагины, включенные в вашу тему, бесплатны, и вы
+                                    можете наслаждаться всеми их функциями. Это здорово!
+                                </p>
+                            </div>
+                        </div>
+
+                        <div class="item">
+                            <div class="item-content">
+                                <div class="icon-wrapper">
+                                    <img src="<?= get_template_directory_uri() ?>/assets/images/supply-lisence/4.svg" alt="image" />
+                                </div>
+                                <h6>Многоязычная поддержка</h6>
+                                <p>
+                                    Перевод готов, вам просто нужно добавить файл перевода.
+                                    Очень легко переключать языки.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</main>
+
+<?php
+get_footer();
+?>
