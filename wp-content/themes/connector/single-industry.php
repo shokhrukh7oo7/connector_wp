2160f1fe1b6077f6b7aa745a53ee8cb2975f0553
shokhrukh7oo7 <shokh7oo70809@gmail.com>
2026-01-09 13:03:29 +0500

    solving it problem detail fixed

---
+++ wp-content/themes/connector/single-industry.php
@@ -0,0 +1,31 @@
+<?php get_header(); ?>
+
+<section class="direction-single">
+    <div class="container">
+
+        <?php if (has_post_thumbnail()): ?>
+            <div class="direction-thumb">
+                <?php the_post_thumbnail('large'); ?>
+            </div>
+        <?php endif; ?>
+
+        <?php if (get_field('icon')): ?>
+            <img src="<?= esc_url(get_field('icon')['url']); ?>" alt="">
+        <?php endif; ?>
+
+        <h1>
+            <?php the_title(); ?>
+        </h1>
+
+        <div class="direction-content">
+            <?php the_content(); ?>
+        </div>
+
+        <a href="<?= esc_url(get_permalink(pll_get_post(42))); ?>">←
+            <?php pll_e('Назад'); ?>
+        </a>
+
+    </div>
+</section>
+
+<?php get_footer(); ?>
