a9f63864b0b17256c449109dd14e03ac6f9deb8a
shokhrukh7oo7 <shokh7oo70809@gmail.com>
2026-01-12 16:44:08 +0500

    installation and setting page finished

--- wp-content/themes/connector/template-parts/installation-and-setting.php
+++ wp-content/themes/connector/template-parts/installation-and-setting.php
@@ -104,202 +104,79 @@ get_header();
 
                     <div class="inst-extra-wrapper-2">
                         <h3>
-                            Настройка программного обеспечения может <br />
-                            потребоваться на следующем оборудовании:
+                            <?= nl2br(get_field('installation_extra_list_header')); ?>
                         </h3>
                         <div class="ins-extra-wrapper-two-content">
-                            <ul>
-                                <li>
-                                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/installation-and-settings/info.svg"
-                                        alt="image" />
-                                    <div>
-                                        <span>Персональные компьютеры, моноблоки и ноутбуки</span>
-                                        <br />
-                                        для установки системного ПО, офисных программ,
-                                        прикладного ПО, драйверов, специализированного ПО и
-                                        антивирусной защиты.
-                                    </div>
-                                </li>
-                                <li>
-                                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/installation-and-settings/info.svg"
-                                        alt="image" />
-                                    <div>
-                                        <span>Серверы </span> <br />
-                                        настройка серверных операционных систем, почтовых
-                                        серверов, файловых серверов, баз данных, гипервизоров
-                                        (виртуализации), интернет-шлюзов, контроллеров домена,
-                                        кластеров, IP телефонии, 1С, DLP-систем, PAM-систем,
-                                        SIEM-систем, СКУД, Norma, центр администрирования
-                                        антивирусным ПО, Zabbix (система мониторинга) и
-                                        резервного копирования. Обновление прошивок контроллеров
-                                        сервера. Конфигурирование RAID массивов.
-                                    </div>
-                                </li>
-                                <li>
-                                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/installation-and-settings/info.svg"
-                                        alt="image" />
-                                    <div>
-                                        <span>Мобильные устройства</span> <br />
-                                        (смартфоны, планшеты) – настройка корпоративных
-                                        приложений и средств безопасности. Настройка
-                                        корпоративной почты. Подключение к корпоративной Wi-Fi
-                                        сети. Авторизация подключения пользователей Wi-Fi через
-                                        SMS.
-                                    </div>
-                                </li>
-                                <li>
-                                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/installation-and-settings/info.svg"
-                                        alt="image" />
-                                    <div>
-                                        <span>Сетевое оборудование</span> <br />
-                                        (маршрутизаторы, коммутаторы, сетевые экраны, точки
-                                        доступа) – настройка встроенного ПО и сервисов
-                                        безопасности. Настройка IP адресации сетей и подсетей.
-                                        Настройка портов. Обновление прошивок.
-                                    </div>
-                                </li>
-                            </ul>
+                            <?php if (have_rows('installation_extra_left_list')): ?>
+                                <ul>
+                                    <?php while (have_rows('installation_extra_left_list')):
+                                        the_row(); ?>
+                                        <?php
+                                        $icon = get_sub_field('icon');
+                                        $header = get_sub_field('header');
+                                        $description = get_sub_field('installation_extra_left_list_text');
+                                        ?>
+                                        <li>
+                                            <img src="<?= esc_url($icon['url']) ?>" alt="<?= esc_attr($icon['alt']) ?>">
+                                            <div>
+                                                <span><?= esc_html($header) ?></span><br>
+                                                <?= esc_html($description) ?>
+                                            </div>
+                                        </li>
+                                    <?php endwhile; ?>
+                                </ul>
+                            <?php endif; ?>
                             <!--  -->
-                            <ul>
-                                <li>
-                                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/installation-and-settings/info.svg"
-                                        alt="image" />
-                                    <div>
-                                        <span>Системы хранения данных</span> <br />
-                                        (NAS, SAN, СХД) – установка ПО для управления данными,
-                                        резервным копированием, организации доступа.
-                                        Конфигурирование RAID массивов. Обновление прошивок.
-                                    </div>
-                                </li>
-                                <li>
-                                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/installation-and-settings/info.svg"
-                                        alt="image" />
-                                    <div>
-                                        <span>Видеонаблюдение и системы безопасности</span>
-                                        <br />
-                                        настройка специализированного программного обеспечения
-                                        на регистраторах и видеосерверах. Конфигурирование HDD.
-                                        Настройка прав доступа. Настройка камер видеонаблюдения.
-                                        Настройка IP адресации сетей и подсетей. Организация
-                                        трансляции на вебсайт. Настройка просмотра камер на
-                                        смартфонах. Настройка ПО GeoVision. Настройка системы
-                                        распознавания автомобильных номеров. Настройка СКУД и
-                                        Face ID. Снятие записей событий. Обновление прошивок.
-                                    </div>
-                                </li>
-                                <li>
-                                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/installation-and-settings/info.svg"
-                                        alt="image" />
-                                    <div>
-                                        <span>Рабочие станции</span> <br />
-                                        для инженерного, графического, дизайнерского и
-                                        аналитического ПО. Системное ПО и драйверы.
-                                    </div>
-                                </li>
-                                <li>
-                                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/installation-and-settings/info.svg"
-                                        alt="image" />
-                                    <div>
-                                        <span>МФУ и периферийные устройства</span> <br />
-                                        установка драйверов, сканирующего/печатающего ПО и
-                                        систем контроля доступа. Настройка печати по сети.
-                                        Обновление прошивок.
-                                    </div>
-                                </li>
-                            </ul>
+                            <?php if (have_rows('installation_extra_right_list')): ?>
+                                <ul>
+                                    <?php while (have_rows('installation_extra_right_list')):
+                                        the_row(); ?>
+                                        <?php
+                                        $icon = get_sub_field('icon');
+                                        $header = get_sub_field('header');
+                                        $description = get_sub_field('installation_extra_right_list_text');
+                                        ?>
+                                        <li>
+                                            <img src="<?= esc_url($icon['url']) ?>" alt="<?= esc_attr($icon['alt']) ?>">
+                                            <div>
+                                                <span><?= esc_html($header) ?></span><br>
+                                                <?= esc_html($description) ?>
+                                            </div>
+                                        </li>
+                                    <?php endwhile; ?>
+                                </ul>
+                            <?php endif; ?>
                         </div>
                     </div>
 
                     <div class="inst-extra-wrapper-banner">
-                        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/installation-and-settings/banner-2.png"
-                            alt="image" />
+                        <?php
+                        $extra_banner_2 = get_field('installation_under_banner');
+                        ?>
+                        <img src="<?= esc_url($extra_banner_2['url']) ?>"
+                            alt="<?= esc_attr($extra_banner_2['alt']) ?>" />
                     </div>
 
                     <div class="partner-content inst-partner-content">
                         <div class="partner-content-header">
-                            <p>Сотрудничество</p>
-                            <h6>Процесс сотрудничества</h6>
+                            <p><?= the_field('cooperation_header'); ?></p>
+                            <h6><?= the_field('cooperation_description'); ?></h6>
                             <h5 class="partner-desc-header">
-                                Для Вашего удобства мы упростили процесс заказа IT <br />
-                                обслуживания
+                                <?= nl2br(get_field('cooperation_description_2')); ?>
                             </h5>
                         </div>
 
                         <div class="partner-content-body">
-                            <ul>
-                                <li>
-                                    Пришлите запрос или свяжитесь с нашим менеджером по
-                                    телефону или Telegram +998 98 365-93-25
-                                </li>
-                                <li>
-                                    Получите консультацию каким образом будут выполняться
-                                    данные работы. На этом этапе можно определить примерную
-                                    стоимость ежемесячной подписки на IT обслуживание.
-                                </li>
-                                <li>
-                                    Согласуйте необходимость выезда нашего IT специалиста на
-                                    осмотр объекта для точного определения объема работ.
-                                </li>
-                                <li>
-                                    Получите коммерческое предложение на работы по
-                                    ежемесячному IT обслуживанию.
-                                </li>
-                                <li>
-                                    После утверждения коммерческого предложения получите
-                                    договор для оплаты в электронном виде в Дидокс.
-                                </li>
-                                <li>Согласуйте необходимость подписания NDA.</li>
-                                <li>
-                                    После оплаты наши менеджеры готовы принимать заявки на
-                                    выполнение работ. Каждая заявка фиксируется в нашей CRM
-                                    системе. В базу данных заносится информация о том кто и
-                                    когда дал заявку, описание задачи и описание решения этой
-                                    задачи, имя специалиста ответственного за выполнения
-                                    заявки. Любая (разумная) заявка берется на контроль и
-                                    обязательно будет выполнена.
-                                </li>
-                                <li>
-                                    Согласуйте необходимость первичного IT аудита. Для
-                                    подробного описания текущей IT инфраструктуры. Данные
-                                    работы входят в стоимость ежемесячной абонентской платы
-                                    (подписки).
-                                </li>
-                                <li>
-                                    Согласуйте необходимость проведения инвентаризации вашего
-                                    IT оборудования. Инвентаризация позволит иметь подробный
-                                    список техники с характеристиками, что позволит определить
-                                    необходимую модернизацию в перспективе. Инвентаризация
-                                    обычно проходит в течении первого месяца обслуживания.
-                                    Данные работы входят в стоимость ежемесячной абонентской
-                                    платы (подписки).
-                                </li>
-                                <li>
-                                    Обычно со стороны Заказчика выделяется ответственный
-                                    менеджер, который дает заявки нашему менеджеру. Так же
-                                    возможно создание группы в Телеграм с добавлением
-                                    сотрудников Заказчика и Исполнителя, тогда заявки
-                                    принимаются прямо в этой группе.
-                                </li>
-                                <li>
-                                    После получения заявки наш менеджер назначает инженера или
-                                    IT специалиста для выполнения этой заявки. Сотрудники
-                                    нашего IT блока/отдела в течении часа (SLA) начинают
-                                    выполнение заявки путем выезда на объект Заказчика или
-                                    дистанционно через сеть Интернет. После выполнения заявки,
-                                    исполнитель фиксирует выполнение заявки в нашей CRM
-                                    системе. В результате формируется база выполненных заявок
-                                    по которой Заказчик всегда может затребовать отчет о
-                                    проделанной работе за любой период времени.
-                                </li>
-                                <li>
-                                    По окончанию каждого месяца обслуживания или 30 дневного
-                                    периода обслуживания (если договор был заключен не 1 числа
-                                    месяца) подписываются счет-фактура и акт выполненных работ
-                                    за этот период IT обслуживания в электронном виде в
-                                    Дидокс. Так же Заказчик делает предоплату за следующий
-                                    месяц обслуживания.
-                                </li>
-                            </ul>
+                            <?php if (have_rows('partner_steps')): ?>
+                                <ul>
+                                    <?php while (have_rows('partner_steps')):
+                                        the_row(); ?>
+                                        <li>
+                                            <?= wp_kses_post(get_sub_field('text')); ?>
+                                        </li>
+                                    <?php endwhile; ?>
+                                </ul>
+                            <?php endif; ?>
                         </div>
                     </div>
                 </div>
