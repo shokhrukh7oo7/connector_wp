b75587dc0cb17541705f956b3a56d5fd4d8182c9
shokhrukh7oo7 <shokh7oo70809@gmail.com>
2026-01-14 14:52:23 +0500

    footer start

--- wp-content/themes/connector/functions.php
+++ wp-content/themes/connector/functions.php
@@ -285,6 +285,17 @@ if (function_exists('acf_add_options_page')) {
 	));
 }
 // ------------------------------------------------------------------------
+if (function_exists('acf_add_options_page')) {
+	acf_add_options_page(array(
+		'page_title' => 'Настройки подвала',
+		'menu_title' => 'Настройки подвала',
+		'menu_slug' => 'footer-settings',
+		'capability' => 'edit_posts',
+		'redirect' => false,
+		'post_id' => 'options_' . pll_current_language()
+	));
+}
+// ------------------------------------------------------------------------
 function register_it_services_post_type()
 {
 	register_post_type('it_service', [
