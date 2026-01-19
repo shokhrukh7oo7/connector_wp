0fa556e640c8e8913c815e51edefbe492e6acd02
shokhrukh7oo7 <shokh7oo70809@gmail.com>
2026-01-19 13:44:53 +0500

    photo gallery finished

--- wp-content/themes/connector/template-parts/photo-gallery.php
+++ wp-content/themes/connector/template-parts/photo-gallery.php
@@ -4,7 +4,18 @@
 */
 get_header();
 ?>
-
+	<div class="container">
+		<div class="photo_gallery_header">
+			<h1>
+				<?= the_field('gallery_header'); ?>
+			</h1>
+		</div>
+		<div class="photo_gallery_content">
+		    <?php
+        		the_content();
+    		?>
+		</div>
+	</div>
 <?php
 get_footer();
 ?>
