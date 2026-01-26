b713d666756938e55a5a68b41232c1e8b4a8aa72
shokhrukh7oo7 <shokh7oo70809@gmail.com>
2026-01-26 14:49:53 +0500

    reload

---
+++ wp-content/themes/connector/template-parts/team-structure.php
@@ -0,0 +1,109 @@
+<?php
+/*
+    Template name: Структура команды
+ */
+get_header();
+?>
+
+<main>
+    <section>
+        <div class="container">
+            <div class="team-structure-photo-wrapper">
+                <h3>Внешний вид</h3>
+                <p>
+                    Встречают по одёжке, а провожают по уму. Не всегда, конечно, наши сотрудники ходят в брендированной
+                    форме, но опрятный внешний вид и определенный десс-код у нас присутствует. Никаких шорт и сандалей
+                    на босую ногу вы не увидите на наших работниках. Опрятный внешний вид и отсутствие излишней
+                    "пахучести", вот наша цель.
+                </p>
+
+                <div class="row">
+                    <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+                        <img src="<?= get_template_directory_uri() . '/assets/images/home/banner.png'; ?>" alt="image">
+                    </div>
+                    <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+                        <img src="<?= get_template_directory_uri() . '/assets/images/home/banner.png'; ?>" alt="image">
+                    </div>
+                    <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+                        <img src="<?= get_template_directory_uri() . '/assets/images/home/banner.png'; ?>" alt="image">
+                    </div>
+                    <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+                        <img src="<?= get_template_directory_uri() . '/assets/images/home/banner.png'; ?>" alt="image">
+                    </div>
+                    <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+                        <img src="<?= get_template_directory_uri() . '/assets/images/home/banner.png'; ?>" alt="image">
+                    </div>
+                    <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+                        <img src="<?= get_template_directory_uri() . '/assets/images/home/banner.png'; ?>" alt="image">
+                    </div>
+                </div>
+            </div>
+
+            <div class="team-structure-photo-wrapper">
+                <h3>Внешний вид</h3>
+                <p>
+                    Встречают по одёжке, а провожают по уму. Не всегда, конечно, наши сотрудники ходят в брендированной
+                    форме, но опрятный внешний вид и определенный десс-код у нас присутствует. Никаких шорт и сандалей
+                    на босую ногу вы не увидите на наших работниках. Опрятный внешний вид и отсутствие излишней
+                    "пахучести", вот наша цель.
+                </p>
+
+                <div class="row">
+                    <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+                        <img src="<?= get_template_directory_uri() . '/assets/images/home/banner.png'; ?>" alt="image">
+                    </div>
+                    <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+                        <img src="<?= get_template_directory_uri() . '/assets/images/home/banner.png'; ?>" alt="image">
+                    </div>
+                    <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+                        <img src="<?= get_template_directory_uri() . '/assets/images/home/banner.png'; ?>" alt="image">
+                    </div>
+                    <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+                        <img src="<?= get_template_directory_uri() . '/assets/images/home/banner.png'; ?>" alt="image">
+                    </div>
+                    <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+                        <img src="<?= get_template_directory_uri() . '/assets/images/home/banner.png'; ?>" alt="image">
+                    </div>
+                    <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+                        <img src="<?= get_template_directory_uri() . '/assets/images/home/banner.png'; ?>" alt="image">
+                    </div>
+                </div>
+            </div>
+
+            <div class="team-structure-photo-wrapper">
+                <h3>Внешний вид</h3>
+                <p>
+                    Встречают по одёжке, а провожают по уму. Не всегда, конечно, наши сотрудники ходят в брендированной
+                    форме, но опрятный внешний вид и определенный десс-код у нас присутствует. Никаких шорт и сандалей
+                    на босую ногу вы не увидите на наших работниках. Опрятный внешний вид и отсутствие излишней
+                    "пахучести", вот наша цель.
+                </p>
+
+                <div class="row">
+                    <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+                        <img src="<?= get_template_directory_uri() . '/assets/images/home/banner.png'; ?>" alt="image">
+                    </div>
+                    <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+                        <img src="<?= get_template_directory_uri() . '/assets/images/home/banner.png'; ?>" alt="image">
+                    </div>
+                    <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+                        <img src="<?= get_template_directory_uri() . '/assets/images/home/banner.png'; ?>" alt="image">
+                    </div>
+                    <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+                        <img src="<?= get_template_directory_uri() . '/assets/images/home/banner.png'; ?>" alt="image">
+                    </div>
+                    <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+                        <img src="<?= get_template_directory_uri() . '/assets/images/home/banner.png'; ?>" alt="image">
+                    </div>
+                    <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+                        <img src="<?= get_template_directory_uri() . '/assets/images/home/banner.png'; ?>" alt="image">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</main>
+
+<?php
+get_footer()
+    ?>
